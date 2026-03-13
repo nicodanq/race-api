dffcfb8dd9230bbbb80df863553ac593b3714ce5
nicolas_danquigny <nicolas.danquigny@outlook.com>
2026-03-13 23:43:40 +0100

    route pour supprimer et début pour modifier

--- src/main/java/com/takima/race/runner/controllers/RunnerController.java
+++ src/main/java/com/takima/race/runner/controllers/RunnerController.java
@@ -2,9 +2,12 @@ package com.takima.race.runner.controllers;
 
 import com.takima.race.runner.entities.Runner;
 import com.takima.race.runner.services.RunnerService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +33,13 @@ public class RunnerController {
     }
 
     @PostMapping
-    public Runner createRunner(Runner runner){
+    public Runner createRunner(@RequestBody Runner runner) {
         return runnerService.createRunner(runner);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteRunner(@PathVariable Long id){
+        runnerService.deleteRunner(id);
+        return "Runner deleted successfully :"+id;
     }
 }
--- src/main/java/com/takima/race/runner/services/RunnerService.java
+++ src/main/java/com/takima/race/runner/services/RunnerService.java
@@ -4,6 +4,11 @@ import com.takima.race.runner.entities.Runner;
 import com.takima.race.runner.repositories.RunnerRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -30,7 +35,21 @@ public class RunnerService {
         );
     }
 
+    // POST /runners
+    @PostMapping
     public Runner createRunner(Runner runner) {
         return runnerRepository.save(runner);
+    }
+
+    // DELETE /runners/{id}
+    @DeleteMapping("/{id}")
+    public void deleteRunner( Long id) {
+        runnerRepository.deleteById(id);
+    }
+
+    // PUT /runners/{id}
+    @PutMapping("/id")
+    public Runner updateRunner(){
+        
     }
 }
