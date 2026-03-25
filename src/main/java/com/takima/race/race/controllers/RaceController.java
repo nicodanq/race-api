320f3cd571920885401236a49591d92eccb1dc5f
nicolas_danquigny <nicolas.danquigny@outlook.com>
2026-03-25 22:29:39 +0100

    feat(race): ajouter RaceController avec GET, POST et count participants

---
+++ src/main/java/com/takima/race/race/controllers/RaceController.java
@@ -0,0 +1,47 @@
+package com.takima.race.race.controllers;
+
+import com.takima.race.race.entities.Race;
+import com.takima.race.race.services.RaceService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/races")
+public class RaceController {
+    private final RaceService raceService;
+
+    public RaceController(RaceService raceService) {
+        this.raceService = raceService;
+    }
+
+    @GetMapping
+    public List<Race> getAll() {
+        return raceService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Race getById(@PathVariable Long id) {
+        return raceService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Race createRace(@RequestBody Race race) {
+        return raceService.createRace(race);
+    }
+
+    @GetMapping("/{raceId}/participants/count")
+    public Map<String, Integer> countParticipants(@PathVariable Long raceId) {
+        return Map.of("count", raceService.countRegisterInRace(raceId));
+    }
+}
